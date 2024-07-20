35b2f362248ac2a23c9ae0b1e060f515fe3e67bd
abhiram945 <abhiram945@gmail.com>
2024-07-20 19:21:36 +0530

    added youtube lectures

--- src/App.jsx
+++ src/App.jsx
@@ -60,14 +60,40 @@ print("Decrypted:", decrypted)
         <main className="mainContainer">
             <div className="expContainer">
                 <div className="headingContainer">
-                    <h1>Exp No : <span>01</span></h1>
-                    <p>- Cipher Text Encryption and Decryption</p>
+                    <p>Exp No: 01 - Cipher Text Encryption and Decryption</p>
                 </div>
                 <div className="codeContainer">
                     <p onClick={(e)=>{setCopy(!copy);navigator.clipboard.writeText(exp1CodeString)}}>{copy ? 'Copied':'Copy code'}</p>
                     <SyntaxHighlighter language="python" style={docco} customStyle={{paddingLeft:'1rem', }}>
                         {exp1CodeString}
                     </SyntaxHighlighter>
+                </div>
+            </div>
+            <h2>Don't have enough time to find the correct video ? We got it...</h2>
+            <div className="videosContainer">
+                <div className="videoInfoContainer">
+                    <p>Topic: Cryptography and types</p>
+                    <div className="videoContainer">
+                        <iframe src="https://www.youtube.com/embed/6_Cxj5WKpIw?si=m_eavxEia78oo4bC" title="YouTube video player" frameborder="0" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div className="videoInfoContainer">
+                    <p>Topic: Caesar Cipher (Part 1)</p>
+                    <div className="videoContainer">
+                        <iframe src="https://www.youtube.com/embed/JtbKh_12ctg?si=PmyCLcLhPlAwXofc" title="YouTube video player" frameborder="0" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div className="videoInfoContainer">
+                    <p>Topic: Caesar Cipher (Part 2)</p>
+                    <div className="videoContainer">
+                        <iframe src="https://www.youtube.com/embed/na5rapg1XsI?si=r13kwEpyLWE8_RW-" title="YouTube video player" frameborder="0" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div className="videoInfoContainer">
+                    <p>Topic: Monoalphabetic Cipher</p>
+                    <div className="videoContainer">
+                        <iframe src="https://www.youtube.com/embed/J-utjSeUq_c?si=Xj8DLEpnjSi0c1BU" title="YouTube video player" frameborder="0" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    </div>
                 </div>
             </div>
         </main>
