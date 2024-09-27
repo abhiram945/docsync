a37d0bac5c419692aa638cfaf3b8e9386e6d0ef3
abhiram945 <abhiram945@gmail.com>
2024-09-27 13:39:55 +0530

    removed passcode

--- src/index.jsx
+++ src/index.jsx
@@ -4,15 +4,10 @@ import Nav from "./Nav";
 import Sc from "./Sc";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const Docsync = () => {
-    const [code, setCode] = useState(-1);
-    useEffect(()=>{
-       const inputCode = prompt("Enter passcode");
-       setCode(inputCode);
-    },[])
     return <>
-            <Nav />
-            {code==1 ? <Sc /> : code!=-1&&<h2 style={{marginTop:'5vh', textAlign:'center', paddingLeft:'1rem', paddingRight:'1rem'}}>Incorrect passcode, refresh the page to try again</h2>}
-        </>
+        <Nav />
+        <Sc />
+    </>
 
 }
 root.render(
